fca94ccdf9b338e64da2e15e62d4e89dfbae027e
Dipannita Shil <dipannitashil24@gmail.com>
2021-02-04 13:29:52 +0530

    jpql samples added

---
+++ src/test/java/com/dipannita/hibernatedemo/repository/JPQLTest.java
@@ -0,0 +1,52 @@
+package com.dipannita.hibernatedemo.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dipannita.hibernatedemo.entity.Course;
+
+@SpringBootTest
+/**
+ * JPQL is used to query entities
+ *
+ */
+public class JPQLTest {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void testJpql_basic() {
+		Query query = em.createQuery("Select c from Course c ");
+		List resultList = query.getResultList();
+		logger.info("Select c from Course c -> {}", resultList);
+	}
+	
+	@Test
+	void testJpql_typed() {
+		TypedQuery<Course> typedQuery = em.createQuery("Select c from Course c ", Course.class);
+		List<Course> resultList = typedQuery.getResultList();
+		logger.info("Select c from Course c -> {}", resultList);
+	}
+	
+	@Test
+	void testJpql_where() {
+		TypedQuery<Course> typedQuery = em.createQuery("Select c from Course c where name like '%h'", Course.class);
+		List<Course> resultList = typedQuery.getResultList();
+		logger.info("Select c from Course c -> {}", resultList);
+	}
+	
+	
+
+}
