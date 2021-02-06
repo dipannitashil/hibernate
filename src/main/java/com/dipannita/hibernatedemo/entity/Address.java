674ea7ef68cf35b55a34ba1a1e4e86e113f93b75
Dipannita Shil <dipannitashil24@gmail.com>
2021-02-06 22:58:40 +0530

    @Enumerated

---
+++ src/main/java/com/dipannita/hibernatedemo/entity/Address.java
@@ -0,0 +1,35 @@
+package com.dipannita.hibernatedemo.entity;
+
+import javax.persistence.Embeddable;
+
+// This is now embeddable in other entities
+@Embeddable
+public class Address {
+	
+	private String line1;
+	private String line2;
+	
+	public Address() {
+		super();
+	}
+	public Address(String line1, String line2) {
+		super();
+		this.line1 = line1;
+		this.line2 = line2;
+	}
+	public String getLine1() {
+		return line1;
+	}
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+	public String getLine2() {
+		return line2;
+	}
+	public void setLine2(String line2) {
+		this.line2 = line2;
+	}
+	
+	
+
+}
